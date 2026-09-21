2f863196ba4ba9b27b05580e51bbcf0c43c39c0e
Colby Moore <cmoore15@uafs.edu>
2026-09-21 10:38:55 -0500

    Started0.1

--- assign4/data/Order.java
+++ assign4/data/Order.java
@@ -1,7 +1,18 @@
 package data;
 
-public class Order {
 
+public class Order {
+	private int numOfOrders = 0;
+	
+	public void countOrder() {
+		this.numOfOrders++;
+	}
+	
+	public String[] order(String order) {
+		String[] newOrder = order.split(",");
+		countOrder();
+		return newOrder;
+	}
 	
 }
 
--- assign4/data/OrderDB.java
+++ assign4/data/OrderDB.java
@@ -1,11 +1,15 @@
 package data;
 
+import java.io.*;
 
 public class OrderDB {
 	
 	
-	public void loadOrders(String fileName){
-
+	public void loadOrders(String fileName) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		while(false) {
+			
+		}
 		
 	}
 	
